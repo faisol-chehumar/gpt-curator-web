5845649a062125c088abb048dd0c1177e823c04b
Faisol Chehumar <faisol.chehumar@gmail.com>
2023-03-01 23:26:03 +0700

    Add Prettier plugin for TailwindCSS and config file

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+// prettier.config.js
+module.exports = {
+  trailingComma: 'es5',
+  printWidth: 80,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+  plugins: [require('prettier-plugin-tailwindcss')],
+};
