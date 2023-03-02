954befa9d4fcad5d1dc783f7461804844b18189b
Faisol Chehumar <faisol.chehumar@gmail.com>
2023-03-02 23:09:38 +0700

    Update page component and page styles to include AppInfo metadata and conic gradient animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
-import Image from 'next/image';
-import { Inter } from 'next/font/google';
-import styles from './page.module.css';
-
-const inter = Inter({ subsets: ['latin'] });
-
 export default function Home() {
   return (
     <h1 className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:py-3 sm:px-8">
