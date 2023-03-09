70fd395b8bb3cd0d15d415aeae5d80cc3c8d4100
Faisol Chehumar <faisol.chehumar@gmail.com>
2023-03-09 22:33:59 +0700

    Add Flex and Highlight components to Hero section in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,27 @@
 import { Layouts, Typography } from '@/shared/components';
 
-const { Container, Section } = Layouts;
-const { HeroHeadline, HeroTagline } = Typography;
+const { Container, Section, Box, Flex } = Layouts;
+const { HeroHeadline, HeroTagline, Highlight } = Typography;
 
 export default function Home() {
   return (
-    <Section data-testid="hero-section" bgColor="dark-primary">
+    <Section data-testid="hero-section" bgColor="dark">
       <Container>
-        <div className="flex h-[40rem] flex-col justify-center lg:w-[500px]">
-          <div>
+        <Flex
+          width={{ mobile: 'full', tablet: 'full', desktop: '500px' }}
+          height={{ mobile: 'fit', tablet: 'fit', desktop: '40rem' }}
+          alignItems="center"
+        >
+          <Box>
             <HeroHeadline color="white">
-              Unlock your creativity with PromptCraft.
+              <Highlight color="primary">Unlock</Highlight> your creativity with
+              PromptCraft.
             </HeroHeadline>
             <HeroTagline color="white">
               Unleash your imagination with our curated prompts
             </HeroTagline>
-          </div>
-        </div>
+          </Box>
+        </Flex>
       </Container>
     </Section>
   );
