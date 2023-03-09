b32d8d504f7a131059e7eab3464a9cbbf7501125
Faisol Chehumar <faisol.chehumar@gmail.com>
2023-03-09 22:35:28 +0700

    Update Container layout to max-w-screen-xl

--- src/shared/components/Layouts/Container/index.tsx
+++ src/shared/components/Layouts/Container/index.tsx
@@ -4,7 +4,7 @@ import { ContainerProps } from '@/shared/components/Layouts/types';
 
 const Container = ({ children }: ContainerProps) => {
   return (
-    <div className="container m-auto max-w-screen-md px-lg lg:max-w-screen-xxl">
+    <div className="container m-auto max-w-screen-md px-lg lg:max-w-screen-xl">
       {children}
     </div>
   );
