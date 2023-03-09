c5314c3f788f69415796cf0b77ac9d992e6c7f9f
Faisol Chehumar <faisol.chehumar@gmail.com>
2023-03-09 22:33:23 +0700

    Add PropsWithChildren and Colors types to commons.ts

---
+++ src/shared/types/commons.ts
@@ -0,0 +1,11 @@
+export interface PropsWithChildren {
+  children: React.ReactNode;
+}
+export type Devices = 'mobile' | 'tablet' | 'desktop';
+export type Colors =
+  | 'transparent'
+  | 'primary'
+  | 'secondary'
+  | 'dark'
+  | 'white'
+  | 'black';
--- src/shared/types/commons.types.ts
+++
@@ -1,3 +0,0 @@
-export interface PropsWithChildren {
-  children: React.ReactNode;
-}
