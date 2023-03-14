31c23e3dc2854933527b627232f14a82b58def6c
Faisol Chehumar <faisol.chehumar@gmail.com>
2023-03-15 06:14:20 +0700

    Add images and carousel component to RecentPromptSection and Trending components

--- src/app/Trending.tsx
+++ src/app/Trending.tsx
@@ -3,13 +3,15 @@
 import React from 'react';
 import Image from 'next/image';
 
-import { Colors, Spaces } from '@/shared/types/commons';
+import { Colors, Sizes, Spaces } from '@/shared/types/commons';
 import Container from '@/shared/components/Layouts/Container';
 import Section from '@/shared/components/Layouts/Section';
 import Title from '@/shared/components/Typography/Title';
 import Carousel from '@/shared/components/Carousel';
+import Box from '@/shared/components/Layouts/Box';
+import { Overflows } from '@/shared/components/Layouts/types';
 
-const RecentPromptSection = () => {
+const Trending = () => {
   const images = [
     { id: '1', image_path: 'http://placekitten.com/g/400/200' },
     { id: '2', image_path: 'http://placekitten.com/g/400/200' },
@@ -24,13 +26,13 @@ const RecentPromptSection = () => {
 
   return (
     <Section
-      testId="recently-added-section"
+      testId="trending-section"
       paddingY={Spaces.LARGE}
       bgColor={Colors.DARKER_DARK}
     >
       <Container>
         <Title color={Colors.WHITE} variant="h2">
-          Recently Added
+          Trending
         </Title>
 
         <Carousel slidesToShows={{ mobile: 2, tablet: 3, desktop: 5 }}>
@@ -50,4 +52,4 @@ const RecentPromptSection = () => {
   );
 };
 
-export default RecentPromptSection;
+export default Trending;
