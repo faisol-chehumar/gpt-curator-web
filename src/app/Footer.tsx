53166ecd6b40224b20d42bc1894450f1c3220582
Faisol Chehumar <faisol.chehumar@gmail.com>
2023-03-16 23:28:15 +0700

    Update Footer component to add Privacy policy and justify content between elements

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 import Box from '@/shared/components/Layouts/Box';
 import Text from '@/shared/components/Typography/Text';
-import { Colors, Sizes, Spaces } from '@/shared/types/commons';
-import { BorderVariants } from '@/shared/components/Layouts/types';
+import { Colors, Spaces } from '@/shared/types/commons';
+import {
+  BorderVariants,
+  JustifyContents,
+} from '@/shared/components/Layouts/types';
 import Section from '@/shared/components/Layouts/Section';
 import Container from '@/shared/components/Layouts/Container';
 import Flex from '@/shared/components/Layouts/Flex';
@@ -17,14 +20,17 @@ const Footer = () => {
       border={{ variant: BorderVariants.BASE_TOP, color: Colors.DARKER_LIGHT }}
     >
       <Container>
-        <Flex>
+        <Flex justifyContent={JustifyContents.BETWEEN}>
           <Box>
             <Text color={Colors.LIGHTER_DARK}>
               Copyright © 2023 PromptCraft Labs Inc.
             </Text>
           </Box>
           <Box>
-            <Text color={Colors.LIGHTER_DARK}>Terms of services</Text>
+            <Flex>
+              <Text color={Colors.LIGHTER_DARK}>Terms of services</Text>
+              <Text color={Colors.LIGHTER_DARK}>Privacy policy</Text>
+            </Flex>
           </Box>
         </Flex>
       </Container>
