3af53ccdd040ba5e71e69c5b674e30994a7ac477
Faisol Chehumar <faisol.chehumar@gmail.com>
2023-03-16 08:12:41 +0700

    Add BorderVariants and update styling for Carousel Item

--- src/shared/components/Carousel/Item.tsx
+++ src/shared/components/Carousel/Item.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { BaseProps, Colors, Sizes, Spaces } from '@/shared/types/commons';
 
 import Box from '../Layouts/Box';
-import { Overflows, Widths } from '../Layouts/types';
+import { BorderVariants, Overflows, Widths } from '../Layouts/types';
 
 const Item: React.FC<BaseProps> = ({ children }) => {
   return (
@@ -12,8 +12,8 @@ const Item: React.FC<BaseProps> = ({ children }) => {
       overflow={Overflows.HIDDEN}
       margin={{ left: Spaces.SMALL, right: Spaces.SMALL }}
       border={{
-        size: Sizes.BASE,
-        color: Colors.LIGHTER_DARK,
+        variant: BorderVariants.BASE_ALL,
+        color: Colors.DARKER_LIGHT,
         radius: Sizes.LARGE,
       }}
     >
