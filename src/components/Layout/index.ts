98122e339ded4efb6cfb0986f72cef0910927423
Faisol Chehumar <faisol.chehumar@gmail.com>
2023-04-30 16:41:48 +0700

    Add Layout components with props for customizing width, height, alignment, padding, margin, border, and overflow

---
+++ src/components/Layout/index.ts
@@ -0,0 +1,13 @@
+import Container from './Container';
+import Section from './Section';
+import Box from './Box';
+import Flex from './Flex';
+
+const Layout = {
+  Container,
+  Section,
+  Box,
+  Flex,
+};
+
+export default Layout;
