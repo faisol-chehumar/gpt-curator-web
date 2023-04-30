3928e510f481b14f51d9d3de1a49e5e6eb841c46
Faisol Chehumar <faisol.chehumar@gmail.com>
2023-04-30 16:45:03 +0700

    Delete Figure component and its related files and types

--- src/shared/components/Figure/classMappings.ts
+++
@@ -1,8 +0,0 @@
-import { FigureVariants } from './types';
-
-export const FIGURE_VARIANTS_MAPS = {
-  [FigureVariants.SMALL]: 'h-[10rem]',
-  [FigureVariants.BASE]: 'h-[15rem]',
-  [FigureVariants.MEDIUM]: 'h-[20rem]',
-  [FigureVariants.LARGE]: 'h-[25rem]',
-};
