d3a4162e6a240e9f6077c5a2e7c401d03a61588b
Faisol Chehumar <faisol.chehumar@gmail.com>
2023-04-30 16:44:49 +0700

    Delete shared components Button classMappings, index, types, Carousel CustomArrow, Item, and index files

--- src/shared/components/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as Layouts } from './Layouts';
-export { default as Typography } from './Typography';
-export { default as Button } from './Button';
-export { default as Carousel } from './Carousel';
-export { default as Svg } from './Svg';
-export { default as Figure } from './Figure';
