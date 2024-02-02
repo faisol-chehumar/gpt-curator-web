cb12efa790a7c663b2c66798d4f3d7c052a6f95c
Faisol Chehumar <faisol.chehumar@gmail.com>
2024-02-03 01:33:22 +0700

    Add image prompt carousel to landing page

--- src/features/landing/index.ts
+++ src/features/landing/index.ts
@@ -1,2 +1,3 @@
 export { default as HeroSection } from './hero-section';
 export { default as AiAssistantSection } from './ai-assistant-section';
+export { default as ImagesPromptCarouselSection } from './images-prompt-carousel-section';
