12af28d8c3204f120e83ad5336c8f3635aa818c8
Faisol Chehumar <faisol.chehumar@gmail.com>
2024-02-19 14:21:45 +0700

    feat(landing page): add generative images carousel

--- src/features/landing/generative-images-carousel.tsx
+++ src/features/landing/generative-images-carousel.tsx
@@ -1,66 +1,66 @@
-import softwareDeveloper from '../../../public/images/software-developer.png';
-import uxuiDesigner from '../../../public/images/graphic-designer.png';
-import investor from '../../../public/images/investor.png';
-import generalAssistant from '../../../public/images/general-assistant.png';
+import { Flex, Heading } from '@/components/ui';
+import { MultiImagesCarousel } from '@/components';
 
-import { Box, Flex, Heading } from '@/components/ui';
-import { LinkCard } from '@/components';
+const ITEMS = [
+  {
+    images: [
+      'https://image.cdn2.seaart.ai/2023-10-05/19049696417850373/1b32fb77eab8f6b3cb94fda58e152702e8df2526_high.webp',
+    ],
+    href: '/images/japanese-anime-style',
+    caption: 'Japanese anime style',
+  },
+  {
+    images: [
+      'https://image.cdn2.seaart.ai/2023-10-05/19049696417850373/1b32fb77eab8f6b3cb94fda58e152702e8df2526_high.webp',
+    ],
+    href: '/images/realistic-architecture',
+    caption: 'Realistic architecture',
+  },
+  {
+    images: [
+      'https://image.cdn2.seaart.ai/2023-10-05/19049696417850373/1b32fb77eab8f6b3cb94fda58e152702e8df2526_high.webp',
+    ],
+    href: '/images/pop-art',
+    caption: 'Pop art',
+  },
+  {
+    images: [
+      'https://image.cdn2.seaart.ai/2023-10-05/19049696417850373/1b32fb77eab8f6b3cb94fda58e152702e8df2526_high.webp',
+    ],
+    href: '/images/illustrator',
+    caption: 'Illustrator',
+  },
+  {
+    images: [
+      'https://image.cdn2.seaart.ai/2023-10-05/19049696417850373/1b32fb77eab8f6b3cb94fda58e152702e8df2526_high.webp',
+    ],
+    href: '/images/illustrator',
+    caption: 'Illustrator',
+  },
+  {
+    images: [
+      'https://image.cdn2.seaart.ai/2023-10-05/19049696417850373/1b32fb77eab8f6b3cb94fda58e152702e8df2526_high.webp',
+    ],
+    href: '/images/illustrator',
+    caption: 'Illustrator',
+  },
+  {
+    images: [
+      'https://image.cdn2.seaart.ai/2023-10-05/19049696417850373/1b32fb77eab8f6b3cb94fda58e152702e8df2526_high.webp',
+    ],
+    href: '/images/illustrator',
+    caption: 'Illustrator',
+  },
+];
 
 const GenerativeImagesCarousel = () => {
   return (
     <>
       <Heading variant="h4" weight="bold" mb={8}>
-        Images prompt
+        Images curated prompt
       </Heading>
-      <Flex direction={{ initial: 'col', md: 'row' }} gap={4}>
-        <Box width={{ initial: 'full', md: '3/12' }} height="min">
-          <LinkCard
-            title="Software developer"
-            subTitle="Help me make great software"
-            src={softwareDeveloper}
-            alt="Young girl coding"
-            href="/assistant/software-developer"
-          />
-        </Box>
-        <Box
-          width={{ initial: 'full', md: '3/12' }}
-          height="min"
-          positions="relative"
-        >
-          <LinkCard
-            title="UX/UI Designer"
-            subTitle="Help me design beautiful app"
-            src={uxuiDesigner}
-            alt="Young girl drawing"
-            href="/assistant/software-developer"
-          />
-        </Box>
-        <Box
-          width={{ initial: 'full', md: '3/12' }}
-          height="min"
-          positions="relative"
-        >
-          <LinkCard
-            title="Investor"
-            subTitle="Advise me professional writings"
-            src={investor}
-            alt="Man show his dashboard"
-            href="/assistant/software-developer"
-          />
-        </Box>
-        <Box
-          width={{ initial: 'full', md: '3/12' }}
-          height="min"
-          positions="relative"
-        >
-          <LinkCard
-            title="General Assistant"
-            subTitle="Help me make great software"
-            src={generalAssistant}
-            alt="Old man typing"
-            href="/assistant/software-developer"
-          />
-        </Box>
+      <Flex justify="center">
+        <MultiImagesCarousel items={ITEMS} />
       </Flex>
     </>
   );
