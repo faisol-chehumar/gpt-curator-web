a609dd0dff9d156a46b034123389147eefaaddf8
Faisol Chehumar <faisol.chehumar@gmail.com>
2024-02-19 16:33:13 +0700

    fix(heading): adjust line height

--- src/components/ui/heading.tsx
+++ src/components/ui/heading.tsx
@@ -14,7 +14,7 @@ type HeadingElement = React.ElementRef<'h1'>;
 const headingVariants = cva('scroll-m-20', {
   variants: {
     variant: {
-      h1: 'text-4xl font-extrabold tracking-tight lg:text-5xl',
+      h1: 'text-4xl font-extrabold tracking-tight lg:text-5xl/[1.2em]',
       h2: 'scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0',
       h3: 'scroll-m-20 text-2xl font-semibold tracking-tight',
       h4: 'scroll-m-20 text-xl font-semibold tracking-tight',
@@ -43,6 +43,7 @@ const Heading = React.forwardRef<HeadingElement, HeadingProps>(
       headingVariants({ variant: Tag }),
       typographyClasses,
       spacingClasses
+      // 'lg:text-8xl/[2rem]'
     );
 
     return (
